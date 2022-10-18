cc68a69c4b40f2964cbdb7bf7c087c60c21db8d1
CHEEWIN BUAPAN <cheewin.b@cmu.ac.th>
2022-10-18 15:00:31 +0700

    add  controller

---
+++ workshop/Controllers/TestController.cs
@@ -0,0 +1,59 @@
+﻿using ITSC_API_GATEWAY_LIB;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using workshop.Model.Entitys;
+
+namespace workshop.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+        }
+
+
+        /// <summary>
+        /// API สำหรับ ดึง User
+        /// </summary>
+        /// <remarks>
+        ///  demo [{OrganizationID:0,BudgetYear:0,BudgetName:0,BudgetCode:0,BudgetReferenceCode:0} , {OrganizationID:0,BudgetYear:0,BudgetName:0,BudgetCode:0,BudgetReferenceCode:0} ]
+        /// </remarks>
+        [HttpGet("v1/User")]
+        [ProducesResponseType(typeof(UserEntity), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> getTest()
+        {
+            UserEntity userEntity = new();
+
+            userEntity.FullName = "test";
+
+            return Ok(userEntity);
+        }
+
+        /// <summary>
+        /// API สำหรับ เพิ่ม User
+        /// </summary>
+        /// <remarks>
+        ///  demo [{OrganizationID:0,BudgetYear:0,BudgetName:0,BudgetCode:0,BudgetReferenceCode:0} , {OrganizationID:0,BudgetYear:0,BudgetName:0,BudgetCode:0,BudgetReferenceCode:0} ]
+        /// </remarks>
+        [HttpPost("v1/User")]
+        [ProducesResponseType(typeof(UserEntity), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> addTest([FromBody] UserEntity model)
+        {
+            UserEntity userEntity = new();
+
+            userEntity.FullName = "test";
+
+            return Ok(userEntity);
+        }
+    }
+
+
+}
--- workshop/Program.cs
+++ workshop/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 using workshop.Model;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,11 +12,56 @@ IConfiguration Configuration = builder.Configuration;
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "E-vote Service",
+        Description = "E-vote Service of CMU",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+    var jwtSecurityScheme = new OpenApiSecurityScheme
+    {
+        Scheme = "bearer",
+        BearerFormat = "Oauth",
+        Name = "Oauth Authentication",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Description = "Put Bearer xxxxx(your token)",
+
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+
+    options.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { jwtSecurityScheme, Array.Empty<string>() }
+    });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("webDB")));
 
 //---------------------------------------------------------------------------------------
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
