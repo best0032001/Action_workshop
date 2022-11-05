ee6bf55f399ae2e175378341762316018c79360c
cheewin.b <cheewin.b@cmu.ac.th>
2022-11-05 14:44:57 +0700

    TestGetDepartment

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -7,24 +7,38 @@ namespace Test
     [TestClass]
     public class UnitTest1
     {
+        DbContextOptionsBuilder<ApplicationDBContext> optionsBuilder;
+        ApplicationDBContext _applicationDBContext;
+        public  UnitTest1()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDBContext>();
+            optionsBuilder.UseSqlServer(@"Server=127.0.0.1;Database=workshop;user id=sa;password=Password[12345];Trust Server Certificate = true");
+            _applicationDBContext = new ApplicationDBContext(optionsBuilder.Options);
+        }
 
         [TestMethod]
         public void TestGetPerson()
         {
+            List<Person> _peoples = _applicationDBContext.People.ToList();
+            Assert.IsTrue(_peoples.Count > 0);
 
-            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDBContext>();
-            // optionsBuilder.UseSqlServer(@"Server=.\;Database=EFCoreWebDemo;Persist Security Info=True;User ID=sa;Password=best_Mfec0032001");
-            optionsBuilder.UseSqlServer(@"Server=127.0.0.1;Database=workshop;user id=sa;password=Password[12345];Trust Server Certificate = true");
+            foreach (Person person in _peoples)
+            {
+                Console.WriteLine("Person : " + person.FullName);
+            }
+ 
+        }
+        [TestMethod]
+        public void TestGetDepartment()
+        {
+          
+            List<Department> _departments = _applicationDBContext.Departments.AsSplitQuery().Include(i => i.Administrator).ToList();
 
-            ApplicationDBContext _applicationDBContext = new ApplicationDBContext(optionsBuilder.Options);
-
-
-            List<Person> _peopleList = _applicationDBContext.People.ToList();
-            Assert.IsTrue(_peopleList.Count > 0);
-
-
-
-
+            foreach (Department department in _departments)
+            {
+                Console.Write("Department : " + department.Name);
+                Console.WriteLine("  Administrator : " + department.Administrator.FullName);
+            }
         }
     }
 }
